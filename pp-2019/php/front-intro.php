cb076691bbd2494ef6e32df2a71eb36ae668a9c5
Katrin Sigurdardottir <katrinduasig@gmail.com>
2019-02-26 09:41:34 +0100

    beginning of pp

---
+++ pp-2019/php/front-intro.php
@@ -0,0 +1,20 @@
+<div class="front-intro">
+    <div class="front-intro-text">
+        <h2>
+            About<span>.</span>
+            Portfolio<span>.</span>
+            Web Development<span>.</span>
+        </h2>
+        <p>Here you can find all relevant information about me and my work.<br> 
+        Don't hesitate to contact me if you have any questions</p>
+    </div>
+    <img id="front-intro-img" src="./assets/front-kds.png" alt="">
+</div>
+
+<script>
+    var imageWidth = document.querySelector('#front-intro-img');
+    var imageHalf = imageWidth.clientWidth/2;
+    var imageWidth = document.querySelector('#front-intro-img');
+    imageWidth.style.right = (screen.width*.2-imageHalf)+'px';
+    
+</script>
---
+++ pp-2019/php/header.php
@@ -0,0 +1,14 @@
+<div class="header">
+    <a href="/" class="logo">
+        <img src="assets/kds_pink.png" alt="">
+        <h1>Katrín Dúa Sigurðardóttir</h1>
+    </a>
+    <ul>
+        <li>
+            <a href="/projects">projects</a>
+        </li>
+        <li>
+            <a href="/CV">cv</a>
+        </li>
+    </ul>
+</div>
